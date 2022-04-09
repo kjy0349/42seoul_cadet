a8bdf37a17da88fdcd5fb6cf708b01ef13fcb6f2
kjy0349 <kjy0349@naver.com>
2022-04-09 21:26:44 +0900

    calloc_edge

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,18 +6,28 @@
 /*   By: jeykim <jeykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 01:02:28 by jeyoung           #+#    #+#             */
-/*   Updated: 2022/04/09 11:37:05 by jeykim           ###   ########.fr       */
+/*   Updated: 2022/04/09 21:23:46 by jeykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	mem_size;
 
+	if (count == 0 || size == 0)
+	{
+		count = 1;
+		size = 1;
+	}
+	mem_size = count * size;
+	if (mem_size / size != count)
+		return (NULL);
 	ptr = malloc(count * size);
-	if (ptr == NULL)
+	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, count * size);
 	return (ptr);
