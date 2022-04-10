de32a57b83d60a7c9c903180e3f9a27fa8e21994
kjy0349 <kjy0349@naver.com>
2022-04-10 18:17:50 +0900

    last_125

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jeykim <jeykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 01:02:28 by jeyoung           #+#    #+#             */
-/*   Updated: 2022/04/09 22:07:16 by jeykim           ###   ########.fr       */
+/*   Updated: 2022/04/10 14:27:10 by jeykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,8 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 	size_t	mem_size;
 
-	if (count == 0 || size == 0)
-	{
-		count = 1;
-		size = 1;
-	}
 	mem_size = count * size;
-	if (mem_size / size != count)
+	if (size != 0 && mem_size / size != count)
 		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
